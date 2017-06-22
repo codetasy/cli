185c56b0e9b2aafed67e28fee21a215f1dc091b7
Jorge Vera <me@jorgevera.net>
2017-06-22 17:13:59 -0400

    Add Cli basic tests

--- src/Codetasy.Cli.Tests/CliTest.cs
+++ src/Codetasy.Cli.Tests/CliTest.cs
@@ -1,18 +1,65 @@
 using System;
 using Xunit;
 using Codetasy.Cli;
+using System.Collections.Generic;
+using System.IO;
 
 namespace Codetasy.Cli.Tests
 {
     public class CliTest
     {
         [Fact]
-        public void CanInstantiate()
+        public void CanExecuteCommand()
         {
-            var args = new string[0];
-            var cli = new Cli(args);
+            var executed = false;
 
-            Assert.IsType<Cli>(cli);
+            var arguments = new [] {"hello"};
+
+            var commands = new Dictionary<string, Action<CliDictionary<string, string>>>();
+            commands.Add("hello", args =>
+            {
+                executed = true;
+            });
+            
+            new Cli(arguments).Execute(commands);
+
+            Assert.True(executed);
+        }
+
+        [Fact]
+        public void CanParseSingleArgument()
+        {
+            var message = string.Empty;
+
+            var arguments = new [] {"hello", "name=Frodo"};
+
+            var commands = new Dictionary<string, Action<CliDictionary<string, string>>>();
+            commands.Add("hello", args =>
+            {
+                message = $"Hello {args["name"]}";
+            });
+            
+            new Cli(arguments).Execute(commands);
+
+            Assert.Equal("Hello Frodo", message);
+        }
+
+        [Fact]
+        public void CanParseMultipleArguments()
+        {
+            var message = string.Empty;
+
+            var arguments = new [] {"hello", "--name=\"Frodo Baggins\"", "--region=\"The Shire\""};
+
+            var commands = new Dictionary<string, Action<CliDictionary<string, string>>>();
+            commands.Add("hello", args =>
+            {
+                message = $"Hello {args["name"]} from {args["region"]}";
+            });
+            
+            new Cli(arguments).Execute(commands);
+
+            Assert.Equal("Hello \"Frodo Baggins\" from \"The Shire\"", message);
         }
     }
 }
